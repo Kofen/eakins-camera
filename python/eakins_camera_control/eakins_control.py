88ba2d87df35a20ebe0425045821307771bd4114
Kofen <Magnus@bryggastudio.no>
2023-08-24 16:13:57 +0200

    removed debug print

--- python/eakins_camera_control/eakins_control.py
+++ python/eakins_camera_control/eakins_control.py
@@ -12,7 +12,6 @@ if os.path.exists('/etc/camera.json'):
     with open('/etc/camera.json', "r") as json_file:
         data["camera"] = json.load(json_file)["camera"]
 
-print(data["camera"])
 def send_command(server, port, command_address, data=None, data_start=None):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((server, port))
